1131917665c1b0d486a2e4653c829d4ed9ddbaf2
Jossan <jocostasan@gmail.com>
2018-10-18 12:09:52 -0300

    add version 2.17.3

--- login/login.py
+++ login/login.py
@@ -23,7 +23,7 @@ class Login(QtGui.QDialog, GUI):
         self.setupUi(self)
         self.iface = iface
         self.loadFields()
-        self.version_lb.setText(u"<b>versão : 2.17.2</b>")
+        self.version_lb.setText(u"<b>versão : 2.17.3</b>")
         self.connectionTypeSlider.valueChanged.connect(
             self.connectionType
         )
