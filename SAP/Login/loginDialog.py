6fb211fbc921157a992866d8d862e2c7ab143cf9
jossanCosta <jossanCosta@gmail.com>
2019-02-21 00:26:55 -0500

    fix bug message load file

--- SAP/Login/loginDialog.py
+++ SAP/Login/loginDialog.py
@@ -18,7 +18,7 @@ class LoginDialog(QtWidgets.QDialog):
         super(LoginDialog, self).__init__()
         self.iface = iface
         uic.loadUi(self.dialog_path, self)
-        self.version_text.setText(u"<b>versão: 3.2.9</b>")
+        self.version_text.setText(u"<b>versão: 3.2.10</b>")
         
     @QtCore.pyqtSlot(int)
     def on_localhost_check_stateChanged(self, state):
