c172e5786f0da8f9b4234b6a8cfe70cedc7a8d95
jossanCosta <jocostasan@gmail.com>
2019-08-15 12:40:27 -0300

    fix bug local routine

--- SAP/Login/loginDialog.py
+++ SAP/Login/loginDialog.py
@@ -18,7 +18,7 @@ class LoginDialog(QtWidgets.QDialog):
         super(LoginDialog, self).__init__()
         self.iface = iface
         uic.loadUi(self.dialog_path, self)
-        self.version_text.setText(u"<b>versão: 3.4.8</b>")
+        self.version_text.setText(u"<b>versão: 3.4.9</b>")
         
     @QtCore.pyqtSlot(int)
     def on_localhost_check_stateChanged(self, state):
