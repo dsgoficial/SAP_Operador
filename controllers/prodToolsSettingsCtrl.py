67c4f357b6ab4b431dba945d9436f608be29156a
jossanCosta <jocostasan@gmail.com>
2020-12-10 09:10:24 -0300

    fix bug shortcut actions

--- controllers/prodToolsSettingsCtrl.py
+++ controllers/prodToolsSettingsCtrl.py
@@ -150,6 +150,7 @@ class ProdToolsSettingsCtrl(ProdToolsCtrl):
     def getMenuBarActionSettings(self):
         iconRootPath = os.path.join(
                 os.path.dirname(__file__),
+                '..',
                 'icons'
         )
         return [
@@ -228,57 +229,24 @@ class ProdToolsSettingsCtrl(ProdToolsCtrl):
             'qgis/digitizing/line_width' : '3',
             'qgis/digitizing/line_color_alpha' : '63',
             'qgis/digitizing/fill_color_alpha' : '40',
-            u'qgis/default_selection_color_alpha': u'63',
-            
-            u'shortcuts/Sair do QGIS' : u'',
-            u'shortcuts/Exit QGIS' : u'',
-            u'shortcuts/Mesclar fei\xe7\xf5es selecionadas' : u'M',
-            u'shortcuts/Merge Selected Features' : u'M',
-            u'shortcuts/Quebrar Fei\xe7\xf5es' : u'C',
-            u'shortcuts/Split Features' : u'C',
-            u'shortcuts/Identificar fei\xe7\xf5es': u'I',
-            u'shortcuts/Identify Features': u'I',
-            u'shortcuts/Adicionar fei\xe7\xe3o': u'A',
-            u'shortcuts/Add Feature': u'A',
-            u'shortcuts/Desfazer sele\xe7\xe3o de fei\xe7\xf5es em todas as camadas': u'D',
-            u'shortcuts/Deselect Features from All Layers': u'D',
-            u'shortcuts/Ferramenta Vértice (Todas as Camadas)' : u'N',
-            u'shortcuts/Vertex Tool (All Layers)' : u'N',
-            u'shortcuts/Salvar para todas as camadas' : u'Ctrl+S',
-            u'shortcuts/Save for All Layers' : u'Ctrl+S',
-            u'shortcuts/Habilitar tra\xe7ar' : u'T',
-            u'shortcuts/Enable Tracing' : u'T',
-            u'shortcuts/Remodelar fei\xe7\xf5es' : u'R',
-            u'shortcuts/Reshape Features' : u'R',
-            u'shortcuts/\xc1rea' : u'Z',
-            u'shortcuts/Measure Area' : u'Z',
-            u'shortcuts/Linha' : u'X',
-            u'shortcuts/Measure Line' : u'X',
-            u'shortcuts/DSGTools: Generic Selector': u'S',
-            u'shortcuts/DSGTools: Seletor Gen\xe9rico': u'S',
-            u'shortcuts/DSGTools: Right Degree Angle Digitizing': u'E',
-            u'shortcuts/DSGTools: Ferramenta de aquisi\xe7\xe3o com \xe2ngulos retos': u'E',
-            'shortcuts/Topological Editing' : 'H',
-            u'shortcuts/Salvar' : u'',
-            u'shortcuts/Save' : u'',
-            u'shortcuts/Select Feature(s)' : u'V',
-            u'shortcuts/Fei\xe7\xe3o(s)' : u'V',
-            u'shortcuts/DSGTools: Inspecionar anterior': u'Q',
-            u'shortcuts/DSGTools: Back Inspect': u'Q',
-            u'shortcuts/DSGTools: Inspecionar pr\xf3ximo': u'W',
-            u'shortcuts/DSGTools: Next Inspect': u'W',
-            u'shortcuts/DSGTools: Desenhar Forma': u'G',
-            u'shortcuts/DSGTools: Draw Shape': u'G',
-            u'shortcuts/Desfazer' : u'',
-            u'shortcuts/Undo' : u'',
-            u'shortcuts/Undo' : u'',
-            u'shortcuts/Mostrar camadas selecionadas' : u'',
-            u'shortcuts/Show Selected Layers' : u'',
-            u'shortcuts/Esconder camadas selecionadas' : u'',
-            u'shortcuts/Hide Selected Layers' : u'',
-            u'shortcuts/Toggle Snapping' : u'',
-            'shortcuts/DSGTools: Toggle all labels visibility' : 'L',
-            u'shortcuts/DSGTools: Ferramenta de Aquisição à Mão Livre' : 'F',
-            'shortcuts/DSGTools: Free Hand Acquisition' : 'F',
-            'shortcuts/DSGTools: Free Hand Reshape' : 'Shift+R'
+            u'qgis/default_selection_color_alpha': u'63'
         }
+
+    def loadCustomQgisShortcuts(self):
+        for setting in self.getShortcutsQgis():
+            self.qgis.updateShortcut(
+                setting['descricao'],
+                setting['shorcut'],
+            )
+
+    def getShortcutsQgis(self):
+        return [
+            {
+                'shorcut': 'Z',
+                'descricao': 'Sair do QGIS'
+            },
+            {
+                'shorcut': 'Z',
+                'descricao': 'Exit QGIS'
+            }
+        ]
