31f5f9088c9f7e12d472f3b76d9f6d0a2a467b3b
jossanCosta <jocostasan@gmail.com>
2021-04-26 09:23:34 -0300

    fix

--- modules/qgis/api/qgisApi.py
+++ modules/qgis/api/qgisApi.py
@@ -53,6 +53,30 @@ class QgisApi(IQgisApi):
 	def removeActionToolBar(self, action):
 		self.customToolBar.removeAction(action)
     
+	def setProjectVariable(self, key, value):
+		chiper_text = base64.b64encode(value.encode('utf-8'))
+		core.QgsExpressionContextUtils.setProjectVariable(
+			core.QgsProject().instance(), 
+			key,
+			chiper_text.decode('utf-8')
+		)
+
+	def getProjectVariable(self, key):
+		current_project  = core.QgsProject().instance()
+		chiper_text = core.QgsExpressionContextUtils.projectScope(current_project).variable(
+			key
+		)
+		value = base64.b64decode(str.encode(chiper_text)).decode('utf-8') if chiper_text else ''
+		return value
+
+	def setSettingsVariable(self, key, value):
+		qsettings = QtCore.QSettings()
+		qsettings.setValue(key, value)
+
+	def getSettingsVariable(self, key):
+		qsettings = QtCore.QSettings()
+		return qsettings.value(key)
+		
     def hasModifiedLayers(self):
         for lyr in core.QgsProject.instance().mapLayers().values():
             if not(lyr.type() == core.QgsMapLayer.VectorLayer):
