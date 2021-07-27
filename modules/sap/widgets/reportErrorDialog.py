4808798d50b59d5e8d673f98da35deb11f98fb61
jossanCosta <jocostasan@gmail.com>
2021-07-27 15:35:37 -0300

    fix

--- modules/sap/widgets/reportErrorDialog.py
+++ modules/sap/widgets/reportErrorDialog.py
@@ -31,6 +31,7 @@ class ReportErrorDialog(SapDialog):
         )
 
     def loadErrorsTypes(self, errorsTypes):
+        self.errorsTypes.clear()
         for item in errorsTypes:
             self.errorsTypesCb.addItem(
                 item['tipo_problema'],
