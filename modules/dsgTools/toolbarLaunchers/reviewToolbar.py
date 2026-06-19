945aef363d1ce5ba86eb3e06fb0a7eb2a8a18aef
dinizime <diniz.ime@gmail.com>
2026-06-19 09:17:22 -0300

    fix reviewtoolbar

--- modules/dsgTools/toolbarLaunchers/reviewToolbar.py
+++ modules/dsgTools/toolbarLaunchers/reviewToolbar.py
@@ -3,7 +3,10 @@ from qgis.utils import plugins
 import json
 
 class ReviewToolBar:
-    
+
+    RANK_FIELD = 'rank'
+    VISITED_FIELD = 'visited'
+
     def __init__(self, controller):
         self.controller = controller
 
@@ -11,16 +14,39 @@ class ReviewToolBar:
         return plugins['DsgTools'].guiManager.productionToolsGuiManager.toolbarsGuiManager.reviewTool
 
     def run(self, gridLayer, outputLayer=None):
+        if not self.validateGridLayer(gridLayer):
+            return None
         if outputLayer is not None and outputLayer.featureCount() > 0:
             self.populateGridLayerWithOutputLayerFeatures(gridLayer, outputLayer)
         reviewToolBar = self.getTool()
         reviewToolBar.setState(
             layer=gridLayer,
-            rankFieldName='rank',
-            visitedFieldName='visited',
+            rankFieldName=self.RANK_FIELD,
+            visitedFieldName=self.VISITED_FIELD,
             zoomType=1,
         )
         return reviewToolBar
+
+    def validateGridLayer(self, gridLayer):
+        if gridLayer is None:
+            return False
+        fieldNames = [field.name() for field in gridLayer.fields()]
+        missingFields = [
+            field
+            for field in (self.RANK_FIELD, self.VISITED_FIELD)
+            if field not in fieldNames
+        ]
+        if missingFields:
+            self.controller.showErrorMessageBox(
+                None,
+                'Erro',
+                'A camada de grid de revisão "{0}" não possui o(s) campo(s) obrigatório(s): {1}. '
+                'Verifique a estrutura da camada antes de iniciar a revisão.'.format(
+                    gridLayer.name(), ', '.join(missingFields)
+                )
+            )
+            return False
+        return True
 
     def populateGridLayerWithOutputLayerFeatures(self, gridLayer, outputLayer):
         gridLayer.startEditing()
